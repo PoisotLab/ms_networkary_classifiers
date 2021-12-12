0e37fe1f63c3c54418b44540912918c756a49cdd
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-12-12 11:14:04 -0500

    update the figures with 64k runs

--- code/03_figures.jl
+++ code/03_figures.jl
@@ -4,6 +4,7 @@ using CSV: CSV
 using DataFramesMeta
 using StatsBase
 using Statistics
+using StatsFuns
 
 results = DataFrame(CSV.File(joinpath(@__DIR__, "output.csv")))
 allowmissing!(results, :value)
@@ -20,7 +21,7 @@ todrop = unique(vcat([
     @subset(results, :measure .== "FPR", :value .== 1.0).runid,
     @subset(results, :measure .== "FNR", :value .== 1.0).runid,
     @subset(results, :measure .== "PRAUC", :value .<= 0.0).runid,
-    @subset(results, :connectance .>= 0.45).runid,
+    @subset(results, :connectance .>= 0.35).runid,
     @subset(results, :connectance .<= 0.05).runid
 ]...))
 
@@ -30,8 +31,8 @@ tokeep(f) = !(f in todrop)
 # Make bins for connectance to make the plotting more efficient
 connectance_values = unique(select(results, [:runid, :connectance]))
 connectance_values.midpoint = zeros(Float64, size(connectance_values,1))
-n_connectance_bins = 4
-bins_connectance = LinRange(0.05, 0.45, n_connectance_bins+1)
+n_connectance_bins = 5
+bins_connectance = LinRange(0.05, 0.35, n_connectance_bins+1)
 for i in 1:n_connectance_bins
     _idx = findall(bins_connectance[i] .<= connectance_values.connectance .< bins_connectance[i+1])
     connectance_values.midpoint[_idx] .= round((bins_connectance[i+1] + bins_connectance[i])/2.0; digits=4)
@@ -73,7 +74,7 @@ data(@subset(results, _keepval.(:measure))) *
 connectance_values = unique(select(results, [:runid, :connectance]))
 connectance_values.conbin = zeros(Float64, size(connectance_values,1))
 n_connectance_bins = 100
-bins_connectance = LinRange(0.05, 0.45, n_connectance_bins+1)
+bins_connectance = LinRange(0.05, 0.35, n_connectance_bins+1)
 for i in 1:n_connectance_bins
     _idx = findall(bins_connectance[i] .<= connectance_values.connectance .< bins_connectance[i+1])
     connectance_values.conbin[_idx] .= (bins_connectance[i+1] + bins_connectance[i])/2.0
