8f54ba9af169318b57026594a47d4534db7d2060
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-02-28 18:19:18 -0500

    new run with some more grid points

--- code/03_figures.jl
+++ code/03_figures.jl
@@ -14,83 +14,54 @@ dropmissing!(results)
 bmm = groupby(results, [:model, :measure, :links, :bias])
 raw = combine(bmm, :value => mean => :value)
 
+# Derived measure of connectance
 raw.connectance = raw.links ./ (100*100)
 
-# Give each run a UUID
-# results.runid = hash.(results.link .* results.bias .* results.bias)
-
-# Make bins for connectance to make the plotting more efficient
-# connectance_values = unique(select(results, [:runid, :connectance]))
-# connectance_values.midpoint = zeros(Float64, size(connectance_values,1))
-# n_connectance_bins = 5
-# bins_connectance = LinRange(0.01, 0.50, n_connectance_bins+1)
-# for i in 1:n_connectance_bins
-#     _idx = findall(bins_connectance[i] .<= connectance_values.connectance .< bins_connectance[i+1])
-#     connectance_values.midpoint[_idx] .= round((bins_connectance[i+1] + bins_connectance[i])/2.0; digits=4)
-# end
-
-# # Join the two dataframes
-# results = leftjoin(results, select(connectance_values, [:runid, :midpoint]), on=:runid)
-
-# With connectance first
+# Show the core measures by connectance
 _keepval(f) = f in ["ROC", "Y", "PR", "mcc"]
-
 dt = data(@subset(raw, _keepval.(:measure)))
 mp = mapping(
     :bias => "Training set bias",
     :value => "Value";
     color=:connectance => "Network connectance",
     row=:measure => "Measure",
-    col=:model => "Model",
+    col=:model => "Model"
 )
-ly = visual(Scatter, colormap = :cividis, markersize = 4)
+ly = visual(Scatter, colormap = :deep, markersize = 4)
 dt * mp * ly |>
     plt -> draw(plt, facet=(;linkyaxes = :minimal)) |>
-    plt -> save(joinpath(@__DIR__, "..", "figures", "bias_combined.png"), plt, px_per_unit = 3)
+    plt -> save(joinpath(@__DIR__, "..", "figures", "bias_by_connectance.png"), plt, px_per_unit = 3)
 
-# Dataviz
-_keepval(f) = f in ["MCC", "INF"]
-data(@subset(results, _keepval.(:measure))) *
-    mapping(:bias => "Training set bias", :value => "Value", row=:midpoint => nonnumeric, col=:model => "Model", color=:measure => "Measure") *
-    (smooth() * visual(linewidth=2.0)) |>
-    plt -> draw(plt, facet=(;linkyaxes = :minimal)) |>
-    plt -> save(joinpath(@__DIR__, "..", "figures", "bias_mcc_inf.png"), plt, px_per_unit = 3)
-
-_keepval(f) = f in ["PRAUC", "ROCAUC"]
-data(@subset(results, _keepval.(:measure))) *
-    mapping(:bias => "Training set bias", :value => "Value", row=:midpoint => nonnumeric, col=:model, color=:measure => "Measure") *
-    (AlgebraOfGraphics.density() * visual(Contour, color=:grey, alpha=0.3) + smooth() * visual(linewidth=2.0)) |>
-    plt -> draw(plt, facet=(;linkyaxes = :minimal)) |>
-    plt -> save(joinpath(@__DIR__, "..", "figures", "bias_roc_pr.png"), plt, px_per_unit = 3)
-
-# Make bins for connectance to get the optimal bias
-connectance_values = unique(select(results, [:runid, :connectance]))
-connectance_values.conbin = zeros(Float64, size(connectance_values,1))
-n_connectance_bins = 100
-bins_connectance = LinRange(0.05, 0.25, n_connectance_bins+1)
-for i in 1:n_connectance_bins
-    _idx = findall(bins_connectance[i] .<= connectance_values.connectance .< bins_connectance[i+1])
-    connectance_values.conbin[_idx] .= (bins_connectance[i+1] + bins_connectance[i])/2.0
-end
-
-results = leftjoin(results, select(connectance_values, [:runid, :conbin]), on=:runid)
-bmm = groupby(results, [:model, :measure, :conbin])
+# Get the best bias for a given connectance
+bmm = groupby(raw, [:model, :measure, :connectance])
 function optival(v,b)
-    idx = sortperm(v)[end-min(50, length(v))+1:end]
-    return (bias=mean(b[idx]), value=mean(v[idx]))
+    idx = argmax(v)
+    return (bias=b[idx], value=v[idx])
 end
 opt = combine(bmm, [:value, :bias] => ((v,b) -> optival(v,b)) => AsTable)
 
-_keepval(f) = f in ["MCC", "INF", "PRAUC", "ROCAUC"]
-data(@subset(opt, _keepval.(:measure))) * 
-    mapping(:conbin => "Network connectance", :bias => "Optimal training set bias", col=:model, row=:measure) *
-    (AlgebraOfGraphics.density() * visual(Contour, color=:grey, alpha=0.3) + smooth() * visual(linewidth=2.0)) |>
-    plt -> draw(plt, facet=(;linkyaxes = :minimal), axis = (xticks = LinearTicks(3),)) |>
-    plt -> save(joinpath(@__DIR__, "..", "figures", "optim_bias.png"), plt, px_per_unit = 3)
+_keepval(f) = f in ["ROC", "Y", "PR", "mcc"]
+dt = data(@subset(opt, _keepval.(:measure)))
+mp = mapping(
+    :connectance => "Connectance",
+    :bias => "Optimal bias";
+    row=:measure => "Measure",
+    col=:model => "Model"
+)
+ly = visual(Scatter, colormap = :deep, markersize = 4)
+dt * mp * ly |>
+    plt -> draw(plt, facet=(;linkyaxes = :minimal)) |>
+    plt -> save(joinpath(@__DIR__, "..", "figures", "optimal_bias.png"), plt, px_per_unit = 3)
 
-_keepval(f) = f in ["MCC", "INF", "PRAUC", "ROCAUC"]
-data(@subset(opt, _keepval.(:measure))) * 
-    mapping(:conbin => "Network connectance", :value => "Validation measure value", col=:model, row=:measure) *
-    (AlgebraOfGraphics.density() * visual(Contour, color=:grey, alpha=0.3) + smooth() * visual(linewidth=2.0)) |>
-    plt -> draw(plt, facet=(;linkyaxes = :minimal), axis = (xticks = LinearTicks(3),)) |>
-    plt -> save(joinpath(@__DIR__, "..", "figures", "optim_perf.png"), plt, px_per_unit = 3)
+_keepval(f) = f in ["ROC", "Y", "PR", "mcc"]
+dt = data(@subset(opt, _keepval.(:measure)))
+mp = mapping(
+    :connectance => "Connectance",
+    :value => "Optimal value";
+    row=:measure => "Measure",
+    col=:model => "Model"
+)
+ly = visual(Scatter, colormap = :deep, markersize = 4)
+dt * mp * ly |>
+    plt -> draw(plt, facet=(;linkyaxes = :minimal)) |>
+    plt -> save(joinpath(@__DIR__, "..", "figures", "optimal_value.png"), plt, px_per_unit = 3)
